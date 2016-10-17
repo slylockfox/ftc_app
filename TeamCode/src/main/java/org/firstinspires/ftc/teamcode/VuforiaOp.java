1b98e4e10ab34a3441356acd653941d6093fb72b
Matt Snyder <slylock2@mac.com>
2016-10-17 00:04:07 -0500

    Following tutorial

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VuforiaOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VuforiaOp.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
 import com.vuforia.HINT;
 import com.vuforia.Vuforia;
 
@@ -21,8 +22,12 @@ import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
 @Autonomous(name="Vuforia test", group = "Test")
 public class VuforiaOp extends LinearOpMode {
 
+    MattHardwarePushbotReverse     robot   = new MattHardwarePushbotReverse();   // Use a Pushbot's hardware
+    private ElapsedTime runtime = new ElapsedTime();
+
     @Override
     public void runOpMode() throws InterruptedException {
+
         VuforiaLocalizer.Parameters params = new VuforiaLocalizer.Parameters(R.id.cameraMonitorViewId);
         params.cameraDirection = VuforiaLocalizer.CameraDirection.FRONT;
         params.vuforiaLicenseKey = "ARda7ET/////AAAAGRKVBrspsk3lhF5JeNbnxGoTXHlCxkXqkmsmgZr7/9G89bz9B9s3pAZj4WcSfAWfuDSgKQ72SgBkTODn75Vektd1v2dqVUC7qCIBX3iVBDm3NxxJbDc8UGRdmWAkjDO+9fxktZiKIdgGKkmLq2OQiYWNKf5zZYBtnJhjH+Rh+Q4gVFb5R+AL5ujKfKV4AQL+nbQ8ow9rRF5reWgTBleibhrYaecm9SEjF209yjAjGWToxd5LlhNEzH3nG/yWBl3kG6ArLNpy/E8WqfvU3UnFpFQ59tOtuE8Y53QQoKCazAiNASN3WLDH5hCMXxh+6tTmHnLpLhKeN+05DgEsD5JnssCdNaea/LWOcYxyqtdqzjQB";
